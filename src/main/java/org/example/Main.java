4d2721df91555372ce63c4be4b2a6ca6f44713d5
Beatriz Ribeiro <1180817@isep.ipp.pt>
2021-11-09 15:35:20 +0000

    Add project setup

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
